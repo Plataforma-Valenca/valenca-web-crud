6df12fab55f203dd2aa8fa67f2b1936ae15158d9
EnzoDangioMota <enzo.mota@germinare.org.br>
2026-02-05 16:51:52 -0300

    feat(Avaliacao): adiciona nova entidade do banco

---
+++ src/main/java/org/example/projetodiogo/model/Avaliacao.java
@@ -0,0 +1,61 @@
+package org.example.projetodiogo.model;
+
+public class Avaliacao {
+    private int id;
+    private String descricao;
+    private Double valor;
+    private int idNota;
+
+    // Método Construtor
+    public Avaliacao(int id, String descricao, Double valor, int idNota) {
+        this.id = id;
+        this.descricao = descricao;
+        this.valor = valor;
+        this.idNota = idNota;
+    }
+
+    // Métodos getters
+    public int getId() {
+        return id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public int getIdNota() {
+        return idNota;
+    }
+
+    // Métodos getters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public void setIdNota(int idNota) {
+        this.idNota = idNota;
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return "Avaliacao{" +
+                "id = " + id +
+                ", descricao = '" + descricao + '\'' +
+                ", valor = " + valor +
+                ", idNota = " + idNota +
+                '}';
+    }
+}
