969df00f3542dade2f4735c3bc15d258f0df76d7
EnzoDangioMota <enzo.mota@germinare.org.br>
2026-02-08 03:05:17 -0300

    feat(ServletLogout.java): implementar servlet de logout

---
+++ src/main/java/org/example/projetodiogo/servlets/ServletLogout.java
@@ -0,0 +1,24 @@
+package org.example.projetodiogo.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebServlet("/logout")
+public class ServletLogout {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+        resp.sendRedirect(req.getContextPath()+"/index.jsp");
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
