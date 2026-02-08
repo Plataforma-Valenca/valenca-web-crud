81357c83554818860ba321227d58a49febaf1739
EnzoDangioMota <enzo.mota@germinare.org.br>
2026-02-08 03:04:17 -0300

    feat(HasherSenha): implementar classe que criptografa as senhas e checa se ela é igual à credencial de login

---
+++ src/main/java/org/example/projetodiogo/util/HasherSenha.java
@@ -0,0 +1,18 @@
+package org.example.projetodiogo.util;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class HasherSenha {
+
+    private final static int tamSenha = 10;
+
+    // Método responsável por fazer o hash da senha original
+    public static String hashSenha(String senha) {
+        return BCrypt.hashpw(senha, BCrypt.gensalt(tamSenha));
+    }
+
+    // Método que verifica se a senha hasheada é compatível com a senha original
+    public static boolean verificaSenha(String senha, String senhaHash) {
+        return BCrypt.checkpw(senha, senhaHash);
+    }
+}
