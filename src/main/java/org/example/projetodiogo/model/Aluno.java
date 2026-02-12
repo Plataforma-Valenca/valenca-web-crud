52753c767c9fd0e224497ea2229c520d2a1712fc
EnzoDangioMota <enzo.mota@germinare.org.br>
2026-02-12 11:11:22 -0300

    feat(Aluno.java): removendo campo não necessário

--- src/main/java/org/example/projetodiogo/model/Aluno.java
+++ src/main/java/org/example/projetodiogo/model/Aluno.java
@@ -3,14 +3,12 @@ package org.example.projetodiogo.model;
 public class Aluno {
 
     private int id;
-    private String nomeCompleto;
     private int matricula;
     private int idUsuario;
 
     // Método Construtor;
-    public Aluno(int id, String nomeCompleto, int matricula, int idUsuario) {
+    public Aluno(int id, int matricula, int idUsuario) {
         this.id = id;
-        this.nomeCompleto = nomeCompleto;
         this.matricula = matricula;
         this.idUsuario = idUsuario;
     }
@@ -19,10 +17,6 @@ public class Aluno {
 
     public int getId() {
         return id;
-    }
-
-    public String getnomeCompleto() {
-        return nomeCompleto;
     }
 
     public int getMatricula() {
@@ -39,10 +33,6 @@ public class Aluno {
         this.id = id;
     }
 
-    public void setnomeCompleto(String nomeCompleto) {
-        this.nomeCompleto = nomeCompleto;
-    }
-
     public void setMatricula(int matricula) {
         this.matricula = matricula;
     }
@@ -57,7 +47,6 @@ public class Aluno {
     public String toString() {
         return "Aluno{" +
                 "id = " + id +
-                ", nomeCompleto = '" + nomeCompleto + '\'' +
                 ", matricula = " + matricula + '\'' +
                 ", idUsuario = " + idUsuario +
                 '}';
