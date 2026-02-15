9d09eb1a6d4d316f72e599c73089c0665d58411c
Enzo Mota <enzodangiomota@gmail.com>
2026-02-15 16:59:02 -0300

    feat(AlunoTurma): criar model da relação de aluno com entrada na turma

---
+++ src/main/java/org/example/projetodiogo/model/AlunoTurma.java
@@ -0,0 +1,46 @@
+package org.example.projetodiogo.model;
+
+import java.sql.Timestamp;
+
+public class AlunoTurma {
+    private int id;
+    private int idTurma;
+    private Timestamp dtEntrada;
+
+    public AlunoTurma() {}
+
+    // Métodos getters
+    public int getId() {
+        return id;
+    }
+
+    public int getIdTurma() {
+        return idTurma;
+    }
+
+    public Timestamp getDtEntrada() {
+        return dtEntrada;
+    }
+
+    // Métodos setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setIdTurma(int idTurma) {
+        this.idTurma = idTurma;
+    }
+
+    public void setDtEntrada(Timestamp dtEntrada) {
+        this.dtEntrada = dtEntrada;
+    }
+
+    // Método toString
+    public String toString() {
+        return "Avaliacao{" +
+                "id = " + id +
+                ", id turma = '" + idTurma + '\'' +
+                ", data entrada = " + dtEntrada +
+                '}';
+    }
+}
