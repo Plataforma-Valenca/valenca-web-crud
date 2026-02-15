f33d3354a3c87bccb7ccd21bf990e55223e52658
Enzo Mota <enzodangiomota@gmail.com>
2026-02-15 17:03:03 -0300

    chore(HasherSenha): alterar nome da variável para workload

--- src/main/java/org/example/projetodiogo/util/HasherSenha.java
+++ src/main/java/org/example/projetodiogo/util/HasherSenha.java
@@ -4,11 +4,11 @@ import org.mindrot.jbcrypt.BCrypt;
 
 public class HasherSenha {
 
-    private final static int tamSenha = 10;
+    private final static int workload = 10;
 
     // Método responsável por fazer o hash da senha original
     public static String hashSenha(String senha) {
-        return BCrypt.hashpw(senha, BCrypt.gensalt(tamSenha));
+        return BCrypt.hashpw(senha, BCrypt.gensalt(workload));
     }
 
     // Método que verifica se a senha hasheada é compatível com a senha original
