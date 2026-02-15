2de7146b75d30a7a950a4801350c5d7da5266c7f
Enzo Mota <enzodangiomota@gmail.com>
2026-02-15 17:05:05 -0300

    refactor(UsuarioDAO): alterar nome do método para usuario ao invés de aluno, corrigir a ordem dos parâmetros, e colocar o campo senha com a senha hasheada no método de atualizar

--- src/main/java/org/example/projetodiogo/dao/UsuarioDAO.java
+++ src/main/java/org/example/projetodiogo/dao/UsuarioDAO.java
@@ -1,11 +1,9 @@
 package org.example.projetodiogo.dao;
 
-import jakarta.security.enterprise.identitystore.PasswordHash;
 import org.example.projetodiogo.exceptions.DataAccessException;
 import org.example.projetodiogo.exceptions.DuplicateEmailException;
 import org.example.projetodiogo.exceptions.EntityNotFoundException;
 import org.example.projetodiogo.exceptions.InvalidCredentialsException;
-import org.example.projetodiogo.model.Observacao;
 import org.example.projetodiogo.model.Usuario;
 import org.example.projetodiogo.util.ConnectionFactory;
 import org.example.projetodiogo.util.HasherSenha;
@@ -20,8 +18,8 @@ import java.util.Optional;
 public class UsuarioDAO {
 
     // CREATE
-    public boolean inserirAluno(Usuario usuario) {
-        String sql = "INSERT INTO usuarios(id, nome, email, senha, tipo, cpf) VALUES(?, ?, ?, ?, ?, ?)";
+    public boolean inserirUsuario(Usuario usuario) {
+        String sql = "INSERT INTO usuarios(nome, email, senha, cpf, tipo) VALUES(?, ?, ?, ?, ?)";
 
         boolean resultado = false;
 
@@ -36,16 +34,15 @@ public class UsuarioDAO {
             // Método para hashear a senha e guardar no banco após a criptografia
             String senhaHash = HasherSenha.hashSenha(usuario.getSenha());
 
-            pstmt.setInt(1, usuario.getId());
-            pstmt.setString(2, usuario.getNome());
-            pstmt.setString(3, usuario.getEmail());
-            pstmt.setString(4, usuario.getSenha());
-            pstmt.setString(5, usuario.getTipoUsuario());
-            pstmt.setString(6, usuario.getCpf());
+            pstmt.setString(1, usuario.getNome());
+            pstmt.setString(2, usuario.getEmail());
+            pstmt.setString(3, senhaHash);
+            pstmt.setString(4, usuario.getCpf());
+            pstmt.setString(5, usuario.getTipoUsuario().toUpperCase());
 
             return (pstmt.executeUpdate() > 0);
         } catch (SQLException e) {
-            System.out.println("[DAO] Erro ao inserir aluno: " + e.getMessage());
+            System.out.println("[DAO] Erro ao inserir usuario: " + e.getMessage());
         } finally {
             try {
                 if (pstmt != null) pstmt.close();
@@ -114,15 +111,17 @@ public class UsuarioDAO {
         String sql = """
                 UPDATE usuarios
                 SET nome = ?, email = ?,  senha = ?
-                WHERE  id = ?
+                WHERE  id_usuario = ?
                 """;
 
         try (Connection conn = ConnectionFactory.conectar();
              PreparedStatement stmt = conn.prepareStatement(sql)) {
 
+            String senhaHash = HasherSenha.hashSenha(usuario.getSenha());
+
             stmt.setString(1, usuario.getNome());
             stmt.setString(2, usuario.getEmail());
-            stmt.setString(3, usuario.getSenha());
+            stmt.setString(3, senhaHash);
             stmt.setInt(4, usuario.getId());
 
             return stmt.executeUpdate() > 0;
