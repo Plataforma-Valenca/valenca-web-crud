5ef70ceca8ab83364aa837d74fbe0f22d08d040d
Enzo Mota <enzodangiomota@gmail.com>
2026-02-15 23:36:17 -0300

    feat(Boletim): criar o model de Boletim

---
+++ src/main/java/org/example/projetodiogo/model/Boletim.java
@@ -0,0 +1,67 @@
+package org.example.projetodiogo.model;
+
+public class Boletim {
+
+    private String nomeDisciplina;
+    private Double media1;
+    private Double media2;
+    private Double mediaFinal;
+    private int idDisciplina;
+
+    // Método construtor
+    public Boletim() {}
+
+    public Boletim(int idDisciplina, String nomeDisciplina, Double media1, Double media2, Double mediaFinal) {
+        this.idDisciplina = idDisciplina;
+        this.nomeDisciplina = nomeDisciplina;
+        this.media1 = media1;
+        this.media2 = media2;
+        this.mediaFinal = mediaFinal;
+    }
+
+    // métodos getters
+    public int getIdDisciplina() {
+        return idDisciplina;
+    }
+
+    public String getNomeDisciplina() {
+        return nomeDisciplina;
+    }
+
+    public Double getMedia1() {
+        return media1;
+    }
+
+    public Double getMedia2() {
+        return media2;
+    }
+
+    public Double getMediaFinal() {
+        return mediaFinal;
+    }
+
+    // Métodos setters
+    public void setIdDisciplina(int idDisciplina) {
+        this.idDisciplina = idDisciplina;
+    }
+
+    public void setNomeDisciplina(String nomeDisciplina) {
+        this.nomeDisciplina = nomeDisciplina;
+    }
+
+    public void setMedia1(Double media1) {
+        this.media1 = media1;
+    }
+
+    public void setMedia2(Double media2) {
+        this.media2 = media2;
+    }
+
+    public void setMediaFinal(Double mediaFinal) {
+        this.mediaFinal = mediaFinal;
+    }
+
+
+
+}
+
