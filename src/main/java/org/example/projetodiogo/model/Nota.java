47a37503c11819665c5956994c63134cb5f3abce
Enzo Mota <enzodangiomota@gmail.com>
2026-02-15 23:49:20 -0300

    refactor(Nota): remover o campo media, idSituacao e seus respectivos métodos

--- src/main/java/org/example/projetodiogo/model/Nota.java
+++ src/main/java/org/example/projetodiogo/model/Nota.java
@@ -4,22 +4,13 @@ public class Nota {
     private int id;
     private int idAluno;
     private int idDisciplina;
-    private Double media;
-    private int idSituacao;
 
     // Método Construtor
     public Nota() {}
 
-    public Nota(int idAluno, int idDisciplina, Double media, int idSituacao) {
+    public Nota(int idAluno, int idDisciplina) {
         this.idAluno = idAluno;
         this.idDisciplina = idDisciplina;
-        this.media = media;
-        this.idSituacao = idSituacao;
-    }
-
-    public Nota(Double media, int idSituacao) {
-        this.media = media;
-        this.idSituacao = idSituacao;
     }
 
     // Métodos getters
@@ -35,10 +26,6 @@ public class Nota {
         return idDisciplina;
     }
 
-    public Double getMedia() { return media; }
-
-    public int getIdSituacao() { return idSituacao; }
-
     // Métodos setters
     public void setId(int id) {
         this.id = id;
@@ -52,10 +39,6 @@ public class Nota {
         this.idDisciplina = idDisciplina;
     }
 
-    public void setMedia(Double media) { this.media = media; }
-
-    public void setIdSituacao(int idSituacao) { this.idSituacao = idSituacao; }
-
     // Método toString
 
     @Override
@@ -63,8 +46,6 @@ public class Nota {
         return "Nota{" +
                 "id = " + id +
                 ", idAluno = " + idAluno +
-                ", media = " + media +
-                ", id situação = " + idSituacao +
                 ", idDisciplina = " + idDisciplina +
                 '}';
     }
