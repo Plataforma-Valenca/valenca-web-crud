a109abf21653e9368f0aecd5937a2168459a2018
EnzoDangioMota <enzo.mota@germinare.org.br>
2026-02-18 23:43:06 -0300

    feat(ValidarPreCadastro.java): implementar servlet de validação do pré-cadastro

---
+++ src/main/java/org/example/projetodiogo/servlets/login/ValidarPreCadastro.java
@@ -0,0 +1,44 @@
+package org.example.projetodiogo.servlets.login;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.example.projetodiogo.dao.AlunoDAO;
+import org.example.projetodiogo.dao.UsuarioDAO;
+import org.example.projetodiogo.model.Aluno;
+import org.example.projetodiogo.model.Usuario;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@WebServlet(value = "/validarPreCadastro")
+public class ValidarPreCadastro {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String matriculaOuCpf = req.getParameter("inputValidacao");
+        Usuario usuario = null;
+        Optional<Aluno> aluno = null;
+        UsuarioDAO usuarioDAO = new UsuarioDAO();
+        AlunoDAO alunoDAO = new AlunoDAO();
+
+
+        if (matriculaOuCpf == null || matriculaOuCpf.trim().isEmpty()) {
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            req.setAttribute("erro", "Campo obrigatório");
+            req.getRequestDispatcher("/WEB-INF/aluno/validarPreCadastro.jsp")
+                    .forward(req, resp);
+        } else {
+            usuario = usuarioDAO.validarPrimeiroAcesso(matriculaOuCpf);
+            if (usuario == null) {
+                resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                req.setAttribute("erro", "Cadastro já realizado ou acesso inválido! Contate a escola.");
+                req.getRequestDispatcher("/index.jsp").forward(req, resp);
+            } else {
+                aluno = alunoDAO.buscarPorIdUsuario(usuario.getId());
+                req.setAttribute("matricula", aluno.get().getMatricula());
+                req.getRequestDispatcher("/WEB-INF/aluno/finalizarCadastroAluno.jsp")
+                        .forward(req, resp);
+            }
+        }
+    }
+}
