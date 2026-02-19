61ac21148d08b1aae3bbf7f9e249da0b86033020
EnzoDangioMota <enzo.mota@germinare.org.br>
2026-02-18 21:40:10 -0300

    feat(FinalizarCadastroAluno.java): implementar servlet para concluir o cadastro do aluno

---
+++ src/main/java/org/example/projetodiogo/servlets/aluno/FinalizarCadastroAluno.java
@@ -0,0 +1,39 @@
+package org.example.projetodiogo.servlets.aluno;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.example.projetodiogo.dao.UsuarioDAO;
+import org.example.projetodiogo.model.Usuario;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@WebServlet(value = "FinalizarCadastroAluno")
+public class FinalizarCadastroAluno {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String matricula = req.getParameter("matricula");
+        String nome = req.getParameter("nome");
+        String email = req.getParameter("email");
+        String senha = req.getParameter("senha");
+        String confirmarSenha = req.getParameter("confirmarSenha");
+        Usuario usuario = null;
+        UsuarioDAO usuarioDAO = new UsuarioDAO();
+        int id = 0;
+
+        if (!senha.equals(confirmarSenha)) {
+            req.setAttribute("erro", "As senhas não coincidem.");
+
+            req.getRequestDispatcher("/WEB-INF/aluno/finalizarCadastroAluno.jsp")
+                    .forward(req, resp);
+        } else {
+            Optional<Usuario> usuarioOpt = usuarioDAO.finalizarCadastro(matricula);
+            if (usuarioOpt.isPresent()) {
+                usuario = new Usuario(usuarioOpt.get().getId(), nome, email, senha, true);
+                usuarioDAO.atualizar(usuario);
+            }
+            resp.sendRedirect(req.getContextPath() + "/index.jsp");
+        }
+    }
+}
