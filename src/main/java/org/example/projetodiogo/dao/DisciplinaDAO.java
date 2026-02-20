09ca5008ec484d371bb93309704491aaaf8b6c98
EnzoDangioMota <enzo.mota@germinare.org.br>
2026-02-20 20:22:52 -0300

    feat(DisciplinaDAO): implementar método para buscar disciplina por id

--- src/main/java/org/example/projetodiogo/dao/DisciplinaDAO.java
+++ src/main/java/org/example/projetodiogo/dao/DisciplinaDAO.java
@@ -1,6 +1,7 @@
 package org.example.projetodiogo.dao;
 
 import org.example.projetodiogo.model.Boletim;
+import org.example.projetodiogo.model.Disciplina;
 import org.example.projetodiogo.util.ConnectionFactory;
 
 import java.sql.Connection;
@@ -73,5 +74,42 @@ public class DisciplinaDAO {
             }
         }
         return null;
+    }
+
+    public Disciplina buscarPorId(int idDisciplina) {
+
+        String sql = """
+                SELECT nome FROM disciplinas
+                WHERE id_disciplina = ?
+    """;
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        Disciplina disciplina = new Disciplina();
+
+        try {
+            conn = ConnectionFactory.conectar();
+            pstmt = conn.prepareStatement(sql);
+
+            pstmt.setInt(1, idDisciplina);
+
+            ResultSet rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                disciplina.setNome(rs.getString("nome"));
+                return disciplina;
+            }
+
+        } catch (SQLException e) {
+            System.out.println("[DAO] Erro ao visualizar o boletim: " + e.getMessage());
+        } finally {
+            try {
+                if (conn != null) ConnectionFactory.desconectar(conn);
+                if (pstmt != null) pstmt.close();
+            } catch (SQLException e) {
+                System.err.println("Erro ao fechar as conexões do Banco: " + e.getMessage());
+            }
+        }
+        return disciplina;
     }
 }
