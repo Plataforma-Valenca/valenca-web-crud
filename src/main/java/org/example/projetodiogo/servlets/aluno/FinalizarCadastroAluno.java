68fee40c827a5e0875c85dd989bdf01bcab291b3
EnzoDangioMota <enzo.mota@germinare.org.br>
2026-02-21 17:43:47 -0300

    feat(FinalizarCadastroAluno): pegar a matrícula inserida na validação de usuario pré cadastrado no sistema e enviar mensagem de sucesso à página de login

--- src/main/java/org/example/projetodiogo/servlets/aluno/FinalizarCadastroAluno.java
+++ src/main/java/org/example/projetodiogo/servlets/aluno/FinalizarCadastroAluno.java
@@ -5,6 +5,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.example.projetodiogo.dao.UsuarioDAO;
 import org.example.projetodiogo.model.Usuario;
 
@@ -14,7 +15,8 @@ import java.util.Optional;
 @WebServlet("/FinalizarCadastroAluno")
 public class FinalizarCadastroAluno extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String matricula = req.getParameter("matricula");
+        HttpSession session = req.getSession(false);
+        String cpfOuMatricula = (String) session.getAttribute("cpfOuMatriculaValidacao");
         String nome = req.getParameter("nome");
         String email = req.getParameter("email");
         String senha = req.getParameter("senha");
@@ -24,17 +26,19 @@ public class FinalizarCadastroAluno extends HttpServlet {
         int id = 0;
 
         if (!senha.equals(confirmarSenha)) {
-            req.setAttribute("erro", "As senhas não coincidem.");
-
+            req.setAttribute("erroLogin", "As senhas não coincidem.");
             req.getRequestDispatcher("/WEB-INF/aluno/finalizarCadastroAluno.jsp")
                     .forward(req, resp);
         } else {
-            Optional<Usuario> usuarioOpt = usuarioDAO.finalizarCadastro(matricula);
+            Optional<Usuario> usuarioOpt = usuarioDAO.buscarPorCpfOuMatricula(cpfOuMatricula);
             if (usuarioOpt.isPresent()) {
                 usuario = new Usuario(usuarioOpt.get().getId(), nome, email, senha, true);
-                usuarioDAO.atualizar(usuario);
+                if (usuarioDAO.atualizarPreCadastro(usuario)) {
+                    req.setAttribute("mensagemSucesso", "Cadastro finalizado com sucesso.");
+                    req.getRequestDispatcher("index.jsp")
+                            .forward(req, resp);
+                }
             }
-            resp.sendRedirect(req.getContextPath() + "/index.jsp");
         }
     }
 }
