be2f905a4319646c7d3015931f1f4b0b1c380e75
EnzoDangioMota <enzo.mota@germinare.org.br>
2026-02-21 11:12:33 -0300

    refactor(AuthFilter): corrgirir as rotas que todos podem acessar

--- src/main/java/org/example/projetodiogo/servlets/login/AuthFilter.java
+++ src/main/java/org/example/projetodiogo/servlets/login/AuthFilter.java
@@ -19,8 +19,8 @@ public class AuthFilter implements Filter {
             String uri = req.getRequestURI();
 
             if (uri.endsWith("index.jsp") ||
-                    uri.contains("ServletLogin") ||
-                    uri.contains("ServletLogout") ||
+                    uri.endsWith("login") ||
+                    uri.endsWith("logout") ||
                     uri.contains("/assets/")) {
 
                 chain.doFilter(request, response);
