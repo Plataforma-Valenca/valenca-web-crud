d86d4d0d9cb63a30e5c7de20df276d347aa2fe75
EnzoDangioMota <enzo.mota@germinare.org.br>
2026-02-21 11:25:13 -0300

    feat(NotasProDsiciplinaServlet): implementar servlet para mostrar as notas de uma matéria específica do aluno

--- src/main/java/org/example/projetodiogo/servlets/aluno/NotasPorDisciplinaServlet.java
+++ src/main/java/org/example/projetodiogo/servlets/aluno/NotasPorDisciplinaServlet.java
@@ -2,6 +2,7 @@ package org.example.projetodiogo.servlets.aluno;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.example.projetodiogo.dao.DisciplinaDAO;
@@ -10,7 +11,7 @@ import org.example.projetodiogo.model.Boletim;
 import java.io.IOException;
 
 @WebServlet("/notasPorDisciplina")
-public class NotasPorDisciplinaServlet {
+public class NotasPorDisciplinaServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         int idUsuario = Integer.parseInt(req.getParameter("idUsuario"));
         int idDisciplina = Integer.parseInt(req.getParameter("idDisciplina"));
