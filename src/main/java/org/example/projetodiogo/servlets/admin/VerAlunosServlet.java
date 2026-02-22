6fefb0cfbdb1cb4cbf8f2cd40cf96d57689e3a32
EnzoDangioMota <enzo.mota@germinare.org.br>
2026-02-22 16:57:21 -0300

    feat(admin/VerAlunoServlet): implementar servlet para listar os alunos

---
+++ src/main/java/org/example/projetodiogo/servlets/admin/VerAlunosServlet.java
@@ -0,0 +1,40 @@
+package org.example.projetodiogo.servlets.admin;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.example.projetodiogo.dao.AlunoConsultaDtoDAO;
+import org.example.projetodiogo.exceptions.DataAccessException;
+import org.example.projetodiogo.model.AlunoConsultaDTO;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/admin/verAlunos")
+public class VerAlunosServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        AlunoConsultaDtoDAO alunoConsultaDao = new AlunoConsultaDtoDAO();
+        List<AlunoConsultaDTO> alunosList;
+
+        String busca = req.getParameter("busca");
+
+        try {
+            if (busca != null && !busca.isEmpty()) {
+                alunosList = alunoConsultaDao.buscarPorMatricula(busca);
+            } else {
+                alunosList = alunoConsultaDao.buscarAlunos();
+            }
+
+            req.setAttribute("alunosList", alunosList);
+            req.setAttribute("busca", busca);
+
+            RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/admin/listarAlunos.jsp");
+            dispatcher.forward(req, resp);
+        } catch (DataAccessException e) {
+            throw new DataAccessException("Erro ao acessar o banco de dados", e);
+        }
+    }
+}
