7d62b3317939bbaa025e23184139ac4f7adde58c
EnzoDangioMota <enzo.mota@germinare.org.br>
2026-02-22 16:56:52 -0300

    refactor(professor/verAlunoServlet): corrigir as rotas e os redirecionamento

--- src/main/java/org/example/projetodiogo/servlets/professor/VerAlunosServlet.java
+++ src/main/java/org/example/projetodiogo/servlets/professor/VerAlunosServlet.java
@@ -16,7 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@WebServlet("/VerAlunos")
+@WebServlet("/professor/verAlunos")
 public class VerAlunosServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         AlunoConsultaDtoDAO alunoConsultaDao = new AlunoConsultaDtoDAO();
@@ -34,7 +34,7 @@ public class VerAlunosServlet extends HttpServlet {
             req.setAttribute("alunosList", alunosList);
             req.setAttribute("busca", busca);
 
-            RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/professor/homeProfessor.jsp");
+            RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/professor/listarAlunos.jsp");
             dispatcher.forward(req, resp);
         } catch (DataAccessException e) {
             throw new DataAccessException("Erro ao acessar o banco de dados", e);
