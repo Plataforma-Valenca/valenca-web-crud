75f6889c9252bacced5b145ab73385f1f3629d80
EnzoDangioMota <enzo.mota@germinare.org.br>
2026-02-22 20:55:30 -0300

    feat(BoletimDAO): mudar o retorno do método de visualizar o boletim completo para List, implementar métodi para visualizar as notas de apenas uma disciplina

--- src/main/java/org/example/projetodiogo/dao/BoletimDAO.java
+++ src/main/java/org/example/projetodiogo/dao/BoletimDAO.java
@@ -1,6 +1,5 @@
 package org.example.projetodiogo.dao;
 
-import org.example.projetodiogo.exceptions.DataAccessException;
 import org.example.projetodiogo.model.Boletim;
 import org.example.projetodiogo.util.ConnectionFactory;
 
@@ -9,11 +8,12 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class BoletimDAO {
-    public ArrayList<Boletim> visualizarBoletim(int idAluno) {
+    public List<Boletim> visualizarBoletim(int idAluno) {
 
-        ArrayList<Boletim> lista = new ArrayList<>();
+        List<Boletim> lista = new ArrayList<>();
 
         String sql = """
         SELECT
@@ -88,4 +88,67 @@ public class BoletimDAO {
         return lista;
     }
 
+    public Boletim visualizarNotasPorDisciplina(int idAluno, int idDisciplina) {
+
+        String sql = """
+        SELECT
+            d.nome,
+        
+            ROUND(COALESCE(AVG(CASE WHEN av.semestre = 1 THEN av.valor END), 0), 2) AS media1,
+            ROUND(COALESCE(AVG(CASE WHEN av.semestre = 2 THEN av.valor END), 0), 2) AS media2,
+        
+            ROUND(
+        		COALESCE(
+                    ((
+                         ROUND(COALESCE(AVG(CASE WHEN av.semestre = 1 THEN av.valor END), 0), 2) +
+                         ROUND(COALESCE(AVG(CASE WHEN av.semestre = 2 THEN av.valor END), 0), 2)
+        			 ) / 2), 0
+            )
+        	, 2) AS media_final
+        
+        FROM disciplinas d
+                 JOIN notas n ON n.id_disciplina = d.id_disciplina
+                 LEFT JOIN avaliacoes av ON av.id_nota = n.id_nota
+        
+        WHERE n.id_aluno = ? AND d.id_disciplina = ?
+        
+        GROUP BY d.nome
+        ORDER BY d.nome;
+    """;
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            conn = ConnectionFactory.conectar();
+            pstmt = conn.prepareStatement(sql);
+
+            pstmt.setInt(1, idAluno);
+            pstmt.setInt(2, idDisciplina);
+
+            ResultSet rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                Boletim boletim = new Boletim(
+                        rs.getInt("id_disciplina"),
+                        rs.getString("nome_disciplina"),
+                        rs.getDouble("media1"),
+                        rs.getDouble("media2"),
+                        rs.getDouble("media_final")
+                );
+                return boletim;
+            }
+
+        } catch (SQLException e) {
+            System.out.println("[DAO] Erro ao visualizar o boletim: " + e.getMessage());
+        } finally {
+            try {
+                if (conn != null) ConnectionFactory.desconectar(conn);
+                if (pstmt != null) pstmt.close();
+            } catch (SQLException e) {
+                System.err.println("Erro ao fechar as conexões do Banco: " + e.getMessage());
+            }
+        }
+        return null;
+    }
 }
