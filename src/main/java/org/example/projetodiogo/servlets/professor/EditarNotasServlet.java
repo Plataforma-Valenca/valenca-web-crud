fd51eca70f899bdded867199e5b65b6df011fb1e
EnzoDangioMota <enzo.mota@germinare.org.br>
2026-02-22 21:49:37 -0300

    feat(EditarNotasServlet): criar classe para o servlet de editar notas

---
+++ src/main/java/org/example/projetodiogo/servlets/professor/EditarNotasServlet.java
@@ -0,0 +1,10 @@
+package org.example.projetodiogo.servlets.professor;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+
+@WebServlet("/professor/editarNotas")
+public class EditarNotasServlet extends HttpServlet {
+
+}
