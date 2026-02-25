c0800471f72960322a6fff9dc66f9371436b5677
EnzoDangioMota <enzo.mota@germinare.org.br>
2026-02-25 17:35:36 -0300

    refactor(/admin/verPerfilAluno): ajustar o servlet de ver o perfil do aluno para pegar id pelo value de um forEach

--- src/main/java/org/example/projetodiogo/servlets/admin/VerPerfilAluno.java
+++ src/main/java/org/example/projetodiogo/servlets/admin/VerPerfilAluno.java
@@ -5,13 +5,9 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import org.example.projetodiogo.dao.*;
 import org.example.projetodiogo.exceptions.DataAccessException;
-import org.example.projetodiogo.model.Aluno;
-import org.example.projetodiogo.model.Boletim;
-import org.example.projetodiogo.model.Observacao;
-import org.example.projetodiogo.model.Usuario;
+import org.example.projetodiogo.model.*;
 
 import java.io.IOException;
 import java.util.List;
@@ -20,27 +16,26 @@ import java.util.Optional;
 @WebServlet("/admin/verPerfilAluno")
 public class VerPerfilAluno extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        HttpSession session = req.getSession();
-        Usuario usuario = (Usuario) session.getAttribute("usuarioLogado");
-        int idUsuario = usuario.getId();
         AlunoDAO alunoDAO = new AlunoDAO();
         BoletimDAO boletimDAO = new BoletimDAO();
         TurmasDAO turmasDAO = new TurmasDAO();
         ObservacaoDAO observacaoDAO = new ObservacaoDAO();
+        AlunoConsultaDtoDAO alunoConsultaDAO = new AlunoConsultaDtoDAO();
+
         List<Boletim> boletimList;
-        int idAlunoParam = Integer.parseInt(req.getParameter("id"));
+        int idAlunoParam = Integer.parseInt(req.getParameter("idAluno"));
 
 
         try {
+            Optional<Aluno> alunoOpt = alunoDAO.buscarPorIdAluno(idAlunoParam);
+
+            AlunoConsultaDTO alunoConsultaDTO = alunoConsultaDAO.buscarPorMatricula(alunoOpt.get().getMatricula());
+
             boletimList = boletimDAO.visualizarBoletim(idAlunoParam);
 
-            List<Observacao> obsList = observacaoDAO.buscarPorIdAluno(idAlunoParam);
-
             req.setAttribute("boletimList", boletimList);
-            req.setAttribute("usuarioNome", usuario.getNome());
-            req.setAttribute("matricula", alunoDAO.buscarPorIdAluno(idAlunoParam).get().getMatricula());
+            req.setAttribute("alunoConsulta", alunoConsultaDTO);
             req.setAttribute("turma", turmasDAO.buscarNomePorIdAluno(idAlunoParam));
-            req.setAttribute("obsList", obsList);
 
             req.getRequestDispatcher("/WEB-INF/professor/perfilAluno.jsp")
                     .forward(req, resp);
