ff716e091032e0d26d4e76906f0a3d45ab5b77a9
EnzoDangioMota <enzo.mota@germinare.org.br>
2026-02-27 23:59:58 -0300

    feat(Boletim): incluir o campo situação e fazer um novo construtor

--- src/main/java/org/example/projetodiogo/model/Boletim.java
+++ src/main/java/org/example/projetodiogo/model/Boletim.java
@@ -7,10 +7,20 @@ public class Boletim {
     private Double media1;
     private Double media2;
     private Double mediaFinal;
+    private String situacao;
     private int idDisciplina;
 
     // Método construtor
     public Boletim() {}
+
+    public Boletim(int idDisciplina, String nomeDisciplina, Double media1, Double media2, Double mediaFinal, String situacao) {
+        this.idDisciplina = idDisciplina;
+        this.nomeDisciplina = nomeDisciplina;
+        this.media1 = media1;
+        this.media2 = media2;
+        this.mediaFinal = mediaFinal;
+        this.situacao = situacao;
+    }
 
     public Boletim(int idDisciplina, String nomeDisciplina, Double media1, Double media2, Double mediaFinal) {
         this.idDisciplina = idDisciplina;
