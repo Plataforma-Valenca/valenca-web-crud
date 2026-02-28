977a2a644847da158957087e80218cde9231ad2b
EnzoDangioMota <enzo.mota@germinare.org.br>
2026-02-28 00:02:41 -0300

    feat(NotasPorDisciplinaServlet): refatorar todo o servlet das notas por disciplina e fazer funcionar

--- src/main/java/org/example/projetodiogo/servlets/aluno/NotasPorDisciplinaServlet.java
+++ src/main/java/org/example/projetodiogo/servlets/aluno/NotasPorDisciplinaServlet.java
@@ -6,27 +6,35 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.example.projetodiogo.dao.AlunoDAO;
+import org.example.projetodiogo.dao.BoletimDAO;
 import org.example.projetodiogo.dao.DisciplinaDAO;
 import org.example.projetodiogo.model.Boletim;
+import org.example.projetodiogo.model.Usuario;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet("/aluno/notasPorDisciplina")
 public class NotasPorDisciplinaServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        int idUsuario = Integer.parseInt(req.getParameter("idUsuario"));
+        HttpSession session = req.getSession();
+        AlunoDAO alunoDAO = new AlunoDAO();
+        BoletimDAO boletimDAO = new BoletimDAO();
+        DisciplinaDAO disciplinaDAO = new DisciplinaDAO();
+        Usuario usuario = (Usuario) session.getAttribute("usuarioLogado");
+        int idAluno = alunoDAO.buscarPorIdUsuario(usuario.getId()).get().getId();
         int idDisciplina = Integer.parseInt(req.getParameter("idDisciplina"));
-        DisciplinaDAO dao = new DisciplinaDAO();
 
-        List<Boletim> boletim = dao.visualizarPorDisciplina(idUsuario, idDisciplina);
+        ArrayList<Boletim> boletimList = boletimDAO.visualizarNotasPorDisciplina(idAluno, idDisciplina);
 
-        req.setAttribute("idUsuario", idUsuario);
         req.setAttribute("idDisciplina", idDisciplina);
-        req.setAttribute("nomeDisciplina", dao.buscarPorId(idDisciplina).getNome());
-        req.setAttribute("boletimList", boletim);
+        req.setAttribute("nomeDisciplina", disciplinaDAO.buscarPorId(idDisciplina).getNome());
+        req.setAttribute("boletimList", boletimList);
 
-        RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/aluno/.jsp");
-        dispatcher.forward(req, resp);
+        req.getRequestDispatcher("/WEB-INF/aluno/notasPorDisciplina.jsp")
+                .forward(req, resp);
     }
 }
