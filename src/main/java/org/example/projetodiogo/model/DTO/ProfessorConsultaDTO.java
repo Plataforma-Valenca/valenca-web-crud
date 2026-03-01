3e3b5c688e02f81583461d42aa5bdeb04b22a9a6
EnzoDangioMota <enzo.mota@germinare.org.br>
2026-03-01 20:04:16 -0300

    feat(ProfessoresConsultaDTO): implementar model para consulta provisória de professores para mostrar ao admin

---
+++ src/main/java/org/example/projetodiogo/model/DTO/ProfessorConsultaDTO.java
@@ -0,0 +1,39 @@
+package org.example.projetodiogo.model.DTO;
+
+public class ProfessorConsultaDTO {
+    private int idUsuario;
+    private String nome;
+    private String email;
+    private String cpf;
+    private String disciplina;
+
+    // construtores
+    public ProfessorConsultaDTO() {}
+
+    public ProfessorConsultaDTO(int idUsuario, String nome, String email, String cpf, String disciplina) {
+        this.idUsuario = idUsuario;
+        this.nome = nome;
+        this.email = email;
+        this.cpf = cpf;
+        this.disciplina = disciplina;
+    }
+
+    // getters
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getDisciplina() {
+        return disciplina;
+    }
+
+
+}
