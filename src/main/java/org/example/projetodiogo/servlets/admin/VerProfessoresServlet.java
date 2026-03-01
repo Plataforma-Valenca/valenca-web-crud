7f8bb4e330750a0e9bccec30dd1c319a574b9c6a
EnzoDangioMota <enzo.mota@germinare.org.br>
2026-03-01 20:05:06 -0300

    feat(VerProfessoresServlet): implementar servlet de ver professores para mostrar ao admin

---
+++ src/main/java/org/example/projetodiogo/servlets/admin/VerProfessoresServlet.java
@@ -0,0 +1,49 @@
+package org.example.projetodiogo.servlets.admin;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.example.projetodiogo.dao.DTO.AlunoConsultaDtoDAO;
+import org.example.projetodiogo.dao.DTO.BuscarProfessoresDtoDAO;
+import org.example.projetodiogo.dao.UsuarioDAO;
+import org.example.projetodiogo.exceptions.DataAccessException;
+import org.example.projetodiogo.model.DTO.AlunoConsultaDTO;
+import org.example.projetodiogo.model.DTO.ProfessorConsultaDTO;
+import org.example.projetodiogo.model.Professor;
+import org.example.projetodiogo.model.Usuario;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/admin/verProfessores")
+public class VerProfessoresServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        BuscarProfessoresDtoDAO buscarProfessoresDtoDAO = new BuscarProfessoresDtoDAO();
+        ArrayList<ProfessorConsultaDTO> professoresList = new ArrayList<>();
+        ProfessorConsultaDTO professor;
+
+        String busca = req.getParameter("busca");
+
+        try {
+            if (busca != null && !busca.isEmpty()) {
+                professor = buscarProfessoresDtoDAO.buscarProfessoresFiltro(busca);
+                if (professor != null) {
+                    professoresList.add(professor);
+                }
+            } else {
+                professoresList = buscarProfessoresDtoDAO.buscarProfessores();
+            }
+
+            req.setAttribute("professoresList", professoresList);
+            req.setAttribute("busca", busca);
+
+            req.getRequestDispatcher("/WEB-INF/admin/listarProfessores.jsp")
+                    .forward(req, resp);
+        } catch (DataAccessException e) {
+            throw new DataAccessException("Erro ao acessar o banco de dados", e);
+        }
+    }
+}
