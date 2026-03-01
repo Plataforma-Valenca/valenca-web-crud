771523d31d208f25bed484a033fc22dc0356c23b
EnzoDangioMota <enzo.mota@germinare.org.br>
2026-03-01 16:20:31 -0300

    feat(GerarBoletimServlet): implementar servlet para gerar o boletim do aluno em pdf

--- src/main/java/org/example/projetodiogo/servlets/aluno/GerarBoletimServlet.java
+++ src/main/java/org/example/projetodiogo/servlets/aluno/GerarBoletimServlet.java
@@ -9,29 +9,37 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.example.projetodiogo.dao.AlunoDAO;
 import org.example.projetodiogo.dao.BoletimDAO;
+import org.example.projetodiogo.dao.DisciplinaDAO;
 import org.example.projetodiogo.model.Aluno;
 import org.example.projetodiogo.model.Boletim;
+import org.example.projetodiogo.model.Disciplina;
+import org.example.projetodiogo.model.Usuario;
+
 import java.awt.*;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@WebServlet("/gerarBoletim")
+@WebServlet("/aluno/gerarBoletim")
 public class GerarBoletimServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        String idUsuario = req.getParameter("idUsuario");
+        HttpSession session = req.getSession();
+        Usuario usuario = (Usuario) session.getAttribute("usuarioLogado");
         AlunoDAO alunoDAO = new AlunoDAO();
-        Optional<Aluno> aluno = alunoDAO.buscarPorIdUsuario(Integer.parseInt(idUsuario));
+        Optional<Aluno> aluno = alunoDAO.buscarPorIdUsuario(usuario.getId());
         BoletimDAO boletimDAO = new BoletimDAO();
+        DisciplinaDAO disciplinaDAO = new DisciplinaDAO();
 
         resp.setContentType("application/pdf");
         resp.setHeader("Content-Disposition", "attachment; filename=boletim.pdf");
 
         List<Boletim> boletimList = boletimDAO.visualizarBoletim(aluno.get().getId());
+        ArrayList<Disciplina> disciplinasList = disciplinaDAO.visualizarDisciplinas();
 
         Document document = new Document();
         try {
@@ -39,7 +47,7 @@ public class GerarBoletimServlet extends HttpServlet {
             document.open();
 
             Font titleFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD, 18);
-            Paragraph titulo = new Paragraph("Boletim", titleFont);
+            Paragraph titulo = new Paragraph(("Boletim - " + usuario.getNome()), titleFont);
             titulo.setAlignment(Element.ALIGN_CENTER);
             document.add(titulo);
             document.add(new Paragraph(" "));
@@ -52,8 +60,10 @@ public class GerarBoletimServlet extends HttpServlet {
             tabela.addCell("N2");
             tabela.addCell("Media Final");
 
-            for (Boletim boletim : boletimList) {
-                tabela.addCell(boletim.getNomeDisciplina());
+            for (int i = 0; i < boletimList.size(); i++) {
+                Boletim boletim = boletimList.get(i);
+                String nomeDisciplina = disciplinasList.get(i).getNome();
+                tabela.addCell(nomeDisciplina);
                 tabela.addCell(String.valueOf(boletim.getMedia1()));
                 tabela.addCell(String.valueOf(boletim.getMedia2()));
                 tabela.addCell(String.valueOf(boletim.getMediaFinal()));
