71bb123dacd7338830be76f2c07e50bc1dc9b362
EnzoDangioMota <enzo.mota@germinare.org.br>
2026-03-03 10:35:25 -0300

    refactor(VerAlunosServlet): corrigir atributos e parametros

--- src/main/java/org/example/projetodiogo/servlets/admin/VerAlunosServlet.java
+++ src/main/java/org/example/projetodiogo/servlets/admin/VerAlunosServlet.java
@@ -6,16 +6,21 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.example.projetodiogo.dao.DTO.AlunoConsultaDtoDAO;
+import org.example.projetodiogo.dao.TurmasDAO;
 import org.example.projetodiogo.exceptions.DataAccessException;
 import org.example.projetodiogo.model.DTO.AlunoConsultaDTO;
+import org.example.projetodiogo.model.Turma;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet("/admin/verAlunos")
 public class VerAlunosServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         AlunoConsultaDtoDAO alunoConsultaDao = new AlunoConsultaDtoDAO();
+        TurmasDAO turmaDAO = new TurmasDAO();
         ArrayList<AlunoConsultaDTO> alunosList = new ArrayList<>();
         AlunoConsultaDTO aluno;
 
@@ -31,12 +36,16 @@ public class VerAlunosServlet extends HttpServlet {
                 alunosList = alunoConsultaDao.buscarAlunos();
             }
 
+            List<Turma> turmas = turmaDAO.buscarTurmas();
+
             req.setAttribute("alunosList", alunosList);
+            // Envia para a JSP
+            req.setAttribute("turmasList", turmas);
             req.setAttribute("busca", busca);
 
             req.getRequestDispatcher("/WEB-INF/admin/listarAlunos.jsp")
                 .forward(req, resp);
-        } catch (DataAccessException e) {
+        } catch (DataAccessException | SQLException e) {
             throw new DataAccessException("Erro ao acessar o banco de dados", e);
         }
     }
