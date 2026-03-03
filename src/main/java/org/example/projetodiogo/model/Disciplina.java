38b486c4a351b6d621f0eb41729fbc8a3b6c6679
EnzoDangioMota <enzo.mota@germinare.org.br>
2026-03-03 15:37:00 -0300

    refactor(Disciplina): remove a getter method in the wrong class

--- src/main/java/org/example/projetodiogo/model/Disciplina.java
+++ src/main/java/org/example/projetodiogo/model/Disciplina.java
@@ -28,11 +28,6 @@ public class Disciplina {
         return idProfessor;
     }
 
-    public String getNomeFormatado() {
-        if (nome == null || nome.isEmpty()) return nome;
-        return nome.substring(0,1).toUpperCase() + nome.substring(1).toLowerCase();
-    }
-
     // Métodos setters
     public void setId(int id) {
         this.id = id;
