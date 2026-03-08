8651bed800b1ccea7fa6ca2f48975cb5cf06c840
Enzo Mota <enzodangiomota@gmail.com>
2026-03-08 13:01:53 -0300

    refactor: correct the query used to link a new student with a class and all the subjects that class has, adapt the students registration type to Long

--- src/main/java/org/example/projetodiogo/dao/AlunoDAO.java
+++ src/main/java/org/example/projetodiogo/dao/AlunoDAO.java
@@ -1,11 +1,11 @@
 package org.example.projetodiogo.dao;
 
 import org.example.projetodiogo.model.Aluno;
-import java.sql.Connection;
+
+import java.sql.*;
+
 import org.example.projetodiogo.util.ConnectionFactory;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+
 import java.util.Optional;
 import org.example.projetodiogo.exceptions.DataAccessException;
 import org.example.projetodiogo.exceptions.EntityNotFoundException;
@@ -32,14 +32,18 @@ public class AlunoDAO {
 
         String sqlNotas = """
         INSERT INTO notas (id_aluno, id_disciplina)
-        SELECT ?, d.id_disciplina
-        FROM disciplinas d
-        AND NOT EXISTS (
+        SELECT a.id_aluno, d.id_disciplina
+        FROM alunos a
+                 JOIN aluno_turma at ON at.id_aluno = a.id_aluno
+                 JOIN turmas t ON t.id_turma = at.id_turma
+                 JOIN disciplinas d ON at.id_turma = t.id_turma
+        WHERE a.id_aluno = ?
+          AND NOT EXISTS (
             SELECT 1
             FROM notas n
-            WHERE n.id_aluno = ?
-            AND n.id_disciplina = d.id_disciplina
-        )
+            WHERE n.id_aluno = a.id_aluno
+              AND n.id_disciplina = d.id_disciplina
+        );
     """;
 
         Connection conn = null;
@@ -59,8 +63,6 @@ public class AlunoDAO {
 
             pstmt2 = conn.prepareStatement(sqlNotas);
             pstmt2.setInt(1, idAluno);
-            pstmt2.setInt(2, idTurma);
-            pstmt2.setInt(3, idAluno);
             pstmt2.executeUpdate();
 
             conn.commit();
@@ -78,27 +80,45 @@ public class AlunoDAO {
     }
 
     // INSERT
-    public void inserir(int idUsuario) {
+    public int inserir(int idUsuario) {
         String query = """
                 INSERT INTO alunos
-                (id_usuario, matricula)
-                VALUES (?, nextval('seq_matricula')::varchar)
+                (id_usuario)
+                VALUES (?)
                 """;
+        int idGeradoUsuario = 0;
 
         Connection conn = null;
         PreparedStatement pstmt = null;
+        ResultSet rs = null;
 
         try {
 
             conn = ConnectionFactory.conectar();
-            pstmt = conn.prepareStatement(query);
+            pstmt = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
 
             pstmt.setInt(1, idUsuario);
 
             pstmt.executeUpdate();
 
+            rs = pstmt.getGeneratedKeys();
+
+            if (rs.next()) {
+                idGeradoUsuario = rs.getInt(1);
+                return idGeradoUsuario;
+            }
+
         } catch (SQLException e) {
             System.out.println("Erro ao inserir aluno: " + e.getMessage());
+        } finally {
+            try {
+                if (pstmt != null) pstmt.close();
+                if (conn != null) ConnectionFactory.desconectar(conn);
+                if (rs != null) rs.close();
+            } catch (SQLException e) {
+                System.err.println("Erro ao fechar as conexões do Banco: " + e.getMessage());
+            }
+            return idGeradoUsuario;
         }
     }
 
