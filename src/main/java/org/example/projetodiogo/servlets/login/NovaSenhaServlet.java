573d009a40a2563ce247a64187f30aac8235945d
Emanuelly Mendes <manuellymendes79@gmail.com>
2026-03-12 22:48:21 -0300

    arrumar servelt

--- src/main/java/org/example/projetodiogo/servlets/login/NovaSenhaServlet.java
+++ src/main/java/org/example/projetodiogo/servlets/login/NovaSenhaServlet.java
@@ -27,7 +27,7 @@ public class NovaSenhaServlet extends HttpServlet {
         }
 
         req.setAttribute("token", token);
-        req.getRequestDispatcher("/WEB-INF/novaSenha.jsp")
+        req.getRequestDispatcher("/WEB-INF/login/novaSenha.jsp")
                 .forward(req, resp);
     }
 
